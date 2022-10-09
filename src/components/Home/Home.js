05528ec18342a34f25ddbe9f3d115fa98918b81b
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-09 19:13:16 +0600

    header

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <h2>This is home compo</h2>
+        </div>
+    );
+};
+
+export default Home;
